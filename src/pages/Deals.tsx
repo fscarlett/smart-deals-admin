905b743fa73f1ce681029a6fc83c2547acf47422
Fox Scarlett <foxontheone@gmail.com>
2026-04-19 20:46:42 -0700

    delete button placement

--- src/pages/Deals.tsx
+++ src/pages/Deals.tsx
@@ -658,7 +658,7 @@ function DealsPage() {
                     ))}
                   </div>
 
-                  <div className='flex shrink-0 items-center gap-2'>
+                  <div className='flex shrink-0 flex-col items-stretch gap-6'>
                     <button
                       type='button'
                       className='rounded-md border border-gray-600 px-3 py-2 text-sm font-medium text-white hover:bg-gray-800'
